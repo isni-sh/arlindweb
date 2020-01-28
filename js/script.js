9dd05f9eefb2179853d8fdba546d026e3b0b32fa
isni-sh <58746922+isni-sh@users.noreply.github.com>
2020-01-28 19:26:39 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// window.addEventListener('scroll', function (e) {
+// if(e.scrollTop === "0px"){
+//     this.document.querySelector('#navbar').style.display = "none"
+// }
+// this.document.querySelector('#navbar').style.display = "inline-block"
+
+//  })
